1d1ec197bb52a5513263d707bb5c80088b1d0ce3
Matt Oestreich <matthewpoestreich@gmail.com>
2025-06-27 11:18:48 -0500

    update

--- tinyvec.hpp
+++ tinyvec.hpp
@@ -4,30 +4,22 @@
 #include <cstddef>
 #include <iostream>
 #include <ostream>
+#include <utility>
 
 template <typename T>
 class tinyvec {
-public:
-  explicit tinyvec<T>()
-      : _data(nullptr),
-        _size(0),
-        _capacity(0){};
+ public:
+  tinyvec<T>() : _data(nullptr), _size(0), _capacity(0){};
 
-  explicit tinyvec<T>(T val)
-      : tinyvec<T>() {
-    push_back(val);
-  };
+  tinyvec<T>(T val) : tinyvec<T>() { push_back(val); };
 
   tinyvec(std::initializer_list<T> init)
-      : _data(new T[init.size()]),
-        _size(init.size()),
-        _capacity(init.size()) {
+      : _data(new T[init.size()]), _size(init.size()), _capacity(init.size()) {
     std::copy(init.begin(), init.end(), _data);
   }
 
-  tinyvec<T>(const tinyvec<T>& other) noexcept
-      : _size(other._size),
-        _capacity(other._capacity) {
+  tinyvec<T>(const tinyvec<T>& other)
+      : _size(other._size), _capacity(other._capacity) {
     if (_capacity > 0) {
       _data = new T[_capacity];
       std::copy(other._data, other._data + _size, _data);
@@ -37,27 +29,19 @@ public:
   };
 
   tinyvec(tinyvec<T>&& other) noexcept
-      : _data(other._data),
-        _size(other._size),
-        _capacity(other._capacity) {
+      : _data(other._data), _size(other._size), _capacity(other._capacity) {
     other._data = nullptr;
     other._size = 0;
     other._capacity = 0;
   }
 
-  ~tinyvec<T>() {
-    delete[] _data;
-  };
+  ~tinyvec<T>() { delete[] _data; };
 
-  size_t size() const {
-    return _size;
-  };
+  size_t size() const { return _size; };
 
-  size_t capacity() const {
-    return _capacity;
-  };
+  size_t capacity() const { return _capacity; };
 
-  void push_back(T& val) {
+  void push_back(const T& val) {
     if (_size >= _capacity) {
       resize();
     }
@@ -82,20 +66,27 @@ public:
   };
 
   T& operator[](size_t index) {
+    if (index >= _size) {
+      throw std::out_of_range("index out of bounds");
+    }
     return _data[index];
   };
 
   const T& operator[](size_t index) const {
+    if (index >= _size) {
+      throw std::out_of_range("index out of bounds");
+    }
     return _data[index];
   };
 
   tinyvec& operator=(const tinyvec& other) noexcept {
     if (this != &other) {
+      T* newData = new T[other._capacity];
+      std::copy(other._data, other._data + other._size, newData);
       delete[] _data;
+      _data = newData;
       _size = other._size;
       _capacity = other._capacity;
-      _data = new T[_capacity];
-      std::copy(other._data, other._data + other._size, _data);
     }
     return *this;
   };
@@ -114,21 +105,21 @@ public:
     return *this;
   }
 
-private:
+ private:
   void resize() {
     size_t cap = (_capacity == 0 ? 1 : _capacity) * 2;
-    T *data = new T[cap];
+    T* data = new T[cap];
 
     for (size_t i = 0; i < _size; ++i) {
-      data[i] = _data[i];
+      data[i] = std::move_if_noexcept(_data[i]);
     }
 
-    delete[] _data; // deallocate old array
+    delete[] _data;  // deallocate old array
     _capacity = cap;
     _data = data;
   };
 
-  T *_data;
+  T* _data;
   size_t _size;
   size_t _capacity;
 };
